a6ddf04e1e90f8941e5553a04d7b0443879e9411
Manny Rocha <mmrocha905@gmail.com>
2023-02-23 13:12:49 -0500

    ______

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,16 +1,15 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
-import noteService from './services/notes'
 import Footer from './components/Footer'
-
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('') 
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
-  
+  const [errorMessage, setErrorMessage] = useState(null)
+
   useEffect(() => {
     noteService
       .getAll()
@@ -28,7 +27,7 @@ const App = () => {
 
     noteService
       .create(noteObject)
-      .then(returnedNote => {
+        .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
@@ -38,35 +37,32 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
-  const toggleImportanceOf = (id) => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-      })
-      .catch(error => {
-        setErrorMessage(          
-          `Note '${note.content}' was already removed from server`        
-        )        
-        setTimeout(() => {          
-          setErrorMessage(null)        
-        }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
-
-
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
 
+   const toggleImportanceOf = id => {
+      const note = notes.find(n => n.id === id)
+      const changedNote = { ...note, important: !note.important }
+  
+      noteService
+        .update(id, changedNote).then(returnedNote => {
+          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        })
+        .catch(error => {
+          setErrorMessage(
+            `Note '${note.content}' was already removed from server`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          setNotes(notes.filter(n => n.id !== id))
+        })
+    }
 
- return (
+  return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes app</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -1,24 +1,23 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
-const noteService = {
-  getAll: async () => {
-    const request = axios.get(baseUrl)
-    const response = await request
-        return response.data
-  },
-
-  create: async newObject => {
-    const request = axios.post(baseUrl, newObject) 
-    const response = await request
-      return response.data
-  },
-
-  update: async (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    const response = await request
-      return response.data
-  }
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
-export default noteService;
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { 
+  getAll, 
+  create, 
+  update 
+}
