e509248f97d644846cd54dcd30ed889731790503
Manny Rocha <mmrocha905@gmail.com>
2023-03-04 14:01:13 -0500

    4.5

--- part3/mongo.js
+++ part3/mongo.js
@@ -7,33 +7,34 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
-const url = 
-    'mongodb+srv://mmrocha905:WFEDZM7pBrdTMCj@fullstackopen3.psvldp5.mongodb.net/noteApp?retryWrites=true&w=majority'
+const url =
+  `mongodb+srv://fullstack:${password}@fullstackopen3.psvldp5.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
-
-
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
+const note = new Note({
+  content: 'CSS is hard',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+/*
 Note.find({}).then(result => {
   result.forEach(note => {
     console.log(note)
   })
   mongoose.connection.close()
-})
-// const note = new Note({
-//   content: 'HTML is Easy',
-//   important: true,
-// })
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+})*/
