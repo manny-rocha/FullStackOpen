deb9707e278d2a213d3518aab280665183e01acf
Manny Rocha <mmrocha905@gmail.com>
2023-03-14 16:00:01 -0400

    styling setup partially complete

--- part5/frontend/bloglist-frontend/src/components/Blog.js
+++ part5/frontend/bloglist-frontend/src/components/Blog.js
@@ -1,50 +1,87 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Blog = (props) => {
-  const blog = props.blog
-  const user = props.user
-  const [blogObject, setBlogObject] = useState(blog)
+const BlogContainer = styled.div`
+  padding-top: 10px;
+  padding-left: 2px;
+  border: solid;
+  border-width: 1px;
+  margin-bottom: 5px;
+`
+
+const BlogHeader = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+const BlogTitle = styled.span`
+  font-size: 20px;
+  font-weight: bold;
+`
+
+const BlogAuthor = styled.span`
+  font-size: 16px;
+  font-style: italic;
+`
+
+const BlogDetails = styled.div`
+  display: ${props => props.visible ? 'block' : 'none'};
+`
+
+const BlogUrl = styled.span`
+  font-size: 16px;
+`
+
+const BlogLikes = styled.span`
+  font-size: 16px;
+`
+
+const BlogAddedBy = styled.span`
+  font-size: 16px;
+`
+
+const BlogButton = styled.button`
+  font-size: 16px;
+`
+
+const Blog = ({ blog, user, updateBlog, deleteBlog }) => {
   const [visible, setVisible] = useState(false)
-  const showWhenVisible = { display: visible ? '' : 'none' }
 
   const toggleVisibility = () => {
     setVisible(!visible)
   }
 
-  const buttonLabel = visible ? 'hide' : 'view'
-
   const increaseLikes = () => {
-    const updatedBlog = ({
+    const updatedBlog = {
       ...blog,
       likes: blog.likes + 1
-    })
-    props.updateBlog(updatedBlog)
-    setBlogObject(updatedBlog)
+    }
+    updateBlog(updatedBlog)
   }
 
-  const removeBlog = () => props.deleteBlog(blog)
-
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
+  const removeBlog = () => {
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
+      deleteBlog(blog)
+    }
   }
 
   return (
-    <div style={blogStyle} className='blog'>
-      <div>
-        <span>{blog.title} - {blog.author} <button id='view-button' onClick={toggleVisibility}>{buttonLabel}</button></span>
-      </div>
-      <div style={showWhenVisible}>
-        <span>{blog.url}</span>
-        <span>{blogObject.likes} <button id='like-button' onClick={increaseLikes}>Like</button></span>
-        <span>Added by: {user.name}</span>
-        <button id='remove-button' onClick={removeBlog}>Remove</button>
-      </div>
-    </div>
+    <BlogContainer>
+      <BlogHeader>
+        <BlogTitle>{blog.title}</BlogTitle>
+        <BlogAuthor>{blog.author}</BlogAuthor>
+        <BlogButton onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</BlogButton>
+      </BlogHeader>
+      <BlogDetails visible={visible}>
+        <BlogUrl>{blog.url}</BlogUrl>
+        <BlogLikes>{blog.likes} <BlogButton onClick={increaseLikes}>Like</BlogButton></BlogLikes>
+        <BlogAddedBy>Added by: {user.name}</BlogAddedBy>
+        {user.username === blog.user.username &&
+          <BlogButton onClick={removeBlog}>Remove</BlogButton>
+        }
+      </BlogDetails>
+    </BlogContainer>
   )
 }
 
